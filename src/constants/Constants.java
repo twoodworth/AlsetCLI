25e5bc7421143c2e1f702037fefcd9593133dc7f
tedwoodworth <theodore.woodworth@gmail.com>
2021-11-02 20:00:27 -0400

    Fix jar file not connecting to database.

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final String EDGAR1_MENU_KEY = "b";
     public static final String EDGAR1_URL = "jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241";
     public static final String WELCOME_SCREEN =
-            "" +
+            "\n" +
                     "+-----------------------------------------------------------------------------------------------------+\n" +
                     "|                                                                                                     |\n" +
                     "|     #########         ##                 #############                            ###############   |\n" +
